61644eab8d1acf933f888fbbb1c2bcd97ba18912
Nomfundo-Phororo <precious.phororo@gmail.com>
2020-12-01 13:04:05 +0200

    the code

---
+++ Patterns/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a max number: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a min number: ");
+            int b= int.Parse(Console.ReadLine());
+            //triangle of 5s
+            for (int i = a; i >=b ; i--)
+            {
+                for (int k = i; k >= 1; k--)
+                {
+                    Console.Write(" " + k+",");
+
+                }
+                Console.WriteLine(" ");
+            }
+       
+            for (int i = b; i <= a; i++)
+            {
+                for (int k = i; k >=1; k--)
+                {
+                    Console.Write(" " + k+",");
+
+                }
+                Console.WriteLine(" ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
